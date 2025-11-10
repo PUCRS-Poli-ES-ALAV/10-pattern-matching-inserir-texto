c65fcd2047e1c4551fdfc805cca4c8e488d16864
FernandoJungLau <fejunglau@gmail.com>
2025-11-10 20:42:10 -0300

    falta arrumar as intruções

--- Enunciado1.py
+++ Enunciado1.py
@@ -1,18 +1,35 @@
+iteracoes = 0
+instrucoes = 0
+
 def procura(s1, s2):
+    global iteracoes
+    global instrucoes
     j = 0
     aux = -1
+    instrucoes += 3
     for i in range(len(s1)):
-        if s2[j] == s1[i]:
+        iteracoes += 1
+        instrucoes += 4
+        if j == len(s2):
+                    return aux
+        if s2[j] == s1[i] or s1[i-1] == s2[j]:
+            instrucoes += 3
             if aux == -1:
+                instrucoes += 1
                 aux = i
+                if s1[i-1] == s2[j]:
+                    aux = i-1
+                    if s2[j] == s1[i]:
+                        j += 1
             j += 1
         else:
+            instrucoes += 2
             j = 0
             aux = -1
-    
+    instrucoes += 1
     return aux
 
-s1 = 'ABCDCBDCBDACBDABDCBADF'
+s1 = 'ABCDCBDCBDAADFCBDABDCBADF'
 s2 = 'ADF'
 
 resposta = procura(s1, s2)
@@ -20,3 +37,5 @@ if(resposta == -1):
     print('s2 não foi encontrado em s1!')
 
 print(f"Primeira ocorrência: {resposta}")
+print(f"Iterações: {iteracoes}")
+print(f"Instruções: {instrucoes}")
